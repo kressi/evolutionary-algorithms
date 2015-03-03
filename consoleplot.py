f025ec141b43d3a2eff79b44bf31526a2c310c5e
kressi <mkressibucher@hotmail.com>
2015-03-03 21:35:29 +0100

    added vega

--- consoleplot.py
+++ consoleplot.py
@@ -12,17 +12,17 @@ def plot(x=[], y=[], size_x=100, size_y=20):
         print('len(x) != len(y) or some elements are negative')
 
     # Scale points such that they fit on canvas
-    scale_x = float(size_x-1)/max(x) if x and max(x) != 0 else size_x
-    scale_y = float(size_y-1)/max(y) if y and max(y) != 0 else size_y
+    scale_x = float(size_x)/max(x) if x and max(x) != 0 else size_x
+    scale_y = float(size_y)/max(y) if y and max(y) != 0 else size_y
     x_scaled = [int(i * scale_x) for i in x]
     y_scaled = [int(i * scale_y) for i in y]
 
     # Create empty canvas
-    canvas = [[' ' for _ in range(size_x)] for _ in range(size_y)]
+    canvas = [[' ' for _ in range(size_x+1)] for _ in range(size_y+1)]
 
     # Add scaled points to canvas
     for ix, iy in zip(x_scaled, y_scaled):
-        canvas[size_y-iy+1][ix] = '*'
+        canvas[size_y-iy][ix] = '*'
 
     # Print rows of canvas
     for row in [''.join(row) for row in canvas]:
