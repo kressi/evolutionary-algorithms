2e79c6f5aa1652a37e3c2a792131a3d830a5696f
Michael Kressibucher <mkressibucher@hotmail.com>
2015-03-10 16:19:29 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,75 +1,53 @@
-# consoleplot.py
-#---------------
+import fcntl
+import os
+import termios
+import struct
 
-def plot(x=None, y=[]):
+
+def plot(x, y, rows=None, columns=None):
     """
-    x, y list of values on x- and y-axis.
-    plot those values within canvas size.
+    x, y list of values on x- and y-axis
+    plot those values within canvas size (rows and columns)
     """
-    if not x:
-        x = range(y)
-
-    if len(x) != len(y):
-        raise AttributeError('x and y do not contain the same number of values')
-
-    # Get Terminal size
-    [size_x, size_y] = getTerminalSize()
+    if not rows or not columns:
+        rows, columns = get_terminal_size()
     # offset for caption
-    size_y -= 5
+    rows -= 4
 
     # Scale points such that they fit on canvas
-    scale_x = float(size_x-1)/(max(x)-min(x)) if x and max(x)-min(x) != 0 else size_x
-    scale_y = float(size_y-1)/(max(y)-min(y)) if y and max(y)-min(y) != 0 else size_y
-    x_scaled = [int((i-min(x)) * scale_x) for i in x]
-    y_scaled = [int((i-min(y)) * scale_y) for i in y]
+    scale_x = float(columns - 1) / \
+        (max(x) - min(x)) if x and max(x) - min(x) != 0 else columns
+    scale_y = float(rows - 1) / \
+        (max(y) - min(y)) if y and max(y) - min(y) != 0 else rows
+    x_scaled = [int((i - min(x)) * scale_x) for i in x]
+    y_scaled = [int((i - min(y)) * scale_y) for i in y]
 
     # Create empty canvas
-    canvas = [[' ' for _ in range(size_x)] for _ in range(size_y)]
+    canvas = [[' ' for _ in range(columns)] for _ in range(rows)]
 
     # Add scaled points to canvas
     for ix, iy in zip(x_scaled, y_scaled):
-        canvas[size_y-iy-1][ix] = '*'
+        canvas[rows - iy - 1][ix] = '*'
 
     # Print rows of canvas
     for row in [''.join(row) for row in canvas]:
         print(row)
 
     # Print scale
-    print('\n', ''.join([
-        'Min x: ',  str(min(x)),
-        ' Max x: ', str(max(x)),
-        ' Min y: ', str(min(y)),
-        ' Max y: ', str(max(y)),
-        '\n'
+    print(''.join([
+        '\nMin x: ', str(min(x)),
+        ' Max x: ',  str(max(x)),
+        ' Min y: ',  str(min(y)),
+        ' Max y: ',  str(max(y))
     ]))
 
 
-def getTerminalSize():
-    """ Source: http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
-    """
-    import os
-    env = os.environ
-    def ioctl_GWINSZ(fd):
-        try:
-            import fcntl, termios, struct, os
-            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
-        except:
-            return
-        return cr
-    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
-    if not cr:
-        try:
-            fd = os.open(os.ctermid(), os.O_RDONLY)
-            cr = ioctl_GWINSZ(fd)
-            os.close(fd)
-        except:
-            pass
-    if not cr:
-        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+def get_terminal_size():
+    try:
+        with open(os.ctermid(), 'r') as fd:
+            rc = struct.unpack(
+                'hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
+    except:
+        rc = (os.getenv('LINES', 25), os.getenv('COLUMNS', 80))
 
-        ### Use get(key[, default]) instead of a try/catch
-        #try:
-        #    cr = (env['LINES'], env['COLUMNS'])
-        #except:
-        #    cr = (25, 80)
-    return int(cr[1]), int(cr[0])
+    return rc
